9fe7f430848e37b5ed328e35ab021d1e186680c9
francojnieva <franco97nieva@hotmail.com>
2024-01-07 19:09:15 -0300

    :art: change component style.

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -11,22 +11,22 @@ const AboutUs = () => {
                 <div className="about-us-cards">
                     <img src={SandraGarcia} alt="Lic. Sandra García Taboada" title='Lic. Sandra García Taboada' className='w-36 rounded-full  shadow-lg' />
                     <div className=' space-y-3'>
-                        <h2 className="text-base">Lic. Sandra García Taboada</h2>
-                        <p className='text-base font-medium'>Licenciada en Psicología, especialista en temas de Salud y Seguridad Social. Doctora en Salud Publica y Especialista en Gestión Organizacional. Jefa de Departamento de Atención Domiciliaria e Inserción Socio Cultural y Laboral de la Dirección General de Salud Mental, Ministerio de Salud del GCBA.</p>
+                        <h2 className="text-lg text-[#173e49]">Lic. Sandra García Taboada</h2>
+                        <p className='text-base font-medium text-[#173e49]'>Licenciada en Psicología, especialista en temas de Salud y Seguridad Social. Doctora en Salud Publica y Especialista en Gestión Organizacional. Jefa de Departamento de Atención Domiciliaria e Inserción Socio Cultural y Laboral de la Dirección General de Salud Mental, Ministerio de Salud del GCBA.</p>
                     </div>
                 </div>
                 <div className="about-us-cards">
                    <img src={MarianaLocatelli} alt="Dra. Mariana Locatelli" title='Dra. Mariana Locatelli' className='w-36 rounded-full shadow-lg' />
                     <div className=' space-y-3'>
-                        <h2 className="text-base">Dra. Mariana Locatelli</h2>
-                        <p className='text-base font-medium'>Especialista en Psiquiatría y Medica Legista. Especialista en Medicina de Enlace. Coordinadora del Área de Dispositivos Residenciales de la Dirección General de Salud Mental, Ministerio de Salud del GCBA. Coordinadora del Programa de Entrenamiento de Operadores Terapéuticos de la DGSAM.</p>
+                        <h2 className="text-lg text-[#173e49]">Dra. Mariana Locatelli</h2>
+                        <p className='text-base font-medium text-[#173e49]'>Especialista en Psiquiatría y Medica Legista. Especialista en Medicina de Enlace. Coordinadora del Área de Dispositivos Residenciales de la Dirección General de Salud Mental, Ministerio de Salud del GCBA. Coordinadora del Programa de Entrenamiento de Operadores Terapéuticos de la DGSAM.</p>
                     </div>
                 </div>
                 <div className="about-us-cards">
                     <img src={VivianaBagaloni} alt="Dra. Viviana Bagaloni Mastrandrea" title='Dra. Viviana Bagaloni Mastrandrea' className='w-36 rounded-full shadow-lg' />
                     <div className=' space-y-3'>
-                        <h2 className="text-base">Dra. Viviana Bagaloni Mastrandrea</h2>
-                        <p className='text-base font-medium'>Abogada con Orientación en Derecho Privado. Cuento con un Posgrado en Derecho de Salud y Responsabilidad Medica e Institucional y una Diplomatura en Contratos Administrativos, Obra Pública y Participación Público-Privada. Asesora Legal de la Dirección General de Salud Mental, Ministerio de Salud del GCBA. Miembro constituyente de la Red Argentina de Abogacía.</p>
+                        <h2 className="text-lg text-[#173e49]">Dra. Viviana Bagaloni Mastrandrea</h2>
+                        <p className='text-base font-medium  text-[#173e49]'>Abogada con Orientación en Derecho Privado. Cuento con un Posgrado en Derecho de Salud y Responsabilidad Medica e Institucional y una Diplomatura en Contratos Administrativos, Obra Pública y Participación Público-Privada. Asesora Legal de la Dirección General de Salud Mental, Ministerio de Salud del GCBA. Miembro constituyente de la Red Argentina de Abogacía.</p>
                     </div>
                 </div>
             </div>
